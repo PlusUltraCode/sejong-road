3b8facfb67d83d2c344f01f79a93dc9f070e4f1b
PlusUltraCode <kkd06144@naver.com>
2024-07-12 16:52:26 +0900

    first init

---
+++ src/main/java/com/example/sejong_road/SejongRoadApplication.java
@@ -0,0 +1,13 @@
+package com.example.sejong_road;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SejongRoadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SejongRoadApplication.class, args);
+	}
+
+}
